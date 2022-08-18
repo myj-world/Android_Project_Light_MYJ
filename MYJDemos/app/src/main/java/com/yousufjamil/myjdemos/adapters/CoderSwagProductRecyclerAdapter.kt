c0d6e936fcaf58f8c731d5dbcd49b50068a6f061
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2022-08-18 20:47:02 +0300

    Added Coderswag Basic Elements

---
+++ MYJDemos/app/src/main/java/com/yousufjamil/myjdemos/adapters/CoderSwagProductRecyclerAdapter.kt
@@ -0,0 +1,7 @@
+package com.yousufjamil.myjdemos.adapters
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+class CoderSwagProductRecyclerAdapter(context: Context): RecyclerView(context) {
+}
