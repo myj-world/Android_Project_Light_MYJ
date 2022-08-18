e5207df9a3cf5a3343f37dbf9b9abf920cb4fee4
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2022-08-18 20:35:40 +0300

    Added Coderswag Basic Activity

---
+++ MYJDemos/app/src/main/java/com/yousufjamil/myjdemos/controller/coderswag/CoderSwagMainActivity.kt
@@ -0,0 +1,12 @@
+package com.yousufjamil.myjdemos.controller.coderswag
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yousufjamil.myjdemos.R
+
+class CoderSwagMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coder_swag_main)
+    }
+}
