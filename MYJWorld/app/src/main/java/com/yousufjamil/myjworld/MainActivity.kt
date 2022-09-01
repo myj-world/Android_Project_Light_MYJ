11f8bd5da01d5ffb8ed06e8e524723f07e85ffc8
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2022-09-01 21:33:29 +0300

    Added logo and splash screen to MYJ World

---
+++ MYJWorld/app/src/main/java/com/yousufjamil/myjworld/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.yousufjamil.myjworld
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed(
+            {
+                val finishLoadingIntent = Intent(this, HomeActivity::class.java)
+                startActivity(finishLoadingIntent)
+            }, 3000
+        )
+    }
+}
