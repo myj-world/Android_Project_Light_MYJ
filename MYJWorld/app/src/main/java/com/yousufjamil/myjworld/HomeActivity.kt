6e97439f5e4f547d92b7a684b63d7a9e05315a35
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2022-09-09 18:27:19 +0300

    Added WebView

--- MYJWorld/app/src/main/java/com/yousufjamil/myjworld/HomeActivity.kt
+++ MYJWorld/app/src/main/java/com/yousufjamil/myjworld/HomeActivity.kt
@@ -5,6 +5,9 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ImageButton
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +20,26 @@ class HomeActivity : AppCompatActivity() {
         if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
             val primaryColorDrawable = ColorDrawable(Color.parseColor("#F47900"))
             supportActionBar?.setBackgroundDrawable(primaryColorDrawable)
+        }
+
+        val webView: WebView = findViewById(R.id.webWeb)
+        val homeBtn: ImageButton = findViewById(R.id.backHomeBtn)
+        val refreshBtn: ImageButton = findViewById(R.id.refreshPageBtn)
+        val url = webView.url
+        val settings = webView.settings
+
+        settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://myj.rf.gd")
+
+        refreshBtn.setOnClickListener {
+            if (url != null) {
+                webView.loadUrl(url)
+            }
+        }
+
+        homeBtn.setOnClickListener {
+            webView.loadUrl("https://myj.rf.gd")
         }
     }
 }
