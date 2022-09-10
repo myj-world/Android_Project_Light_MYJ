f2297f5d9640518c5e3bc42f0b61ddd19dc91c12
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2022-09-10 12:04:51 +0300

    Completed WebView

--- MYJWorld/app/src/main/java/com/yousufjamil/myjworld/HomeActivity.kt
+++ MYJWorld/app/src/main/java/com/yousufjamil/myjworld/HomeActivity.kt
@@ -1,13 +1,20 @@
 package com.yousufjamil.myjworld
 
 import android.content.res.Configuration
+import android.graphics.Bitmap
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
-import androidx.appcompat.app.AppCompatActivity
+import android.net.http.SslError
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnTouchListener
+import android.webkit.SslErrorHandler
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ImageButton
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,15 +29,20 @@ class HomeActivity : AppCompatActivity() {
             supportActionBar?.setBackgroundDrawable(primaryColorDrawable)
         }
 
+        val loadWebProgress = findViewById<ProgressBar>(R.id.loadWebProgress)
         val webView: WebView = findViewById(R.id.webWeb)
         val homeBtn: ImageButton = findViewById(R.id.backHomeBtn)
         val refreshBtn: ImageButton = findViewById(R.id.refreshPageBtn)
+        var webpageTitleTxt: TextView = findViewById(R.id.webpageTitleTxt)
         val url = webView.url
         val settings = webView.settings
 
+        loadWebProgress.visibility = View.INVISIBLE
+        settings.setDomStorageEnabled(true)
         settings.javaScriptEnabled = true
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://myj.rf.gd")
+        webView.loadUrl("https://www.myj.rf.gd")
+        webView.canGoBack()
 
         refreshBtn.setOnClickListener {
             if (url != null) {
@@ -40,6 +52,37 @@ class HomeActivity : AppCompatActivity() {
 
         homeBtn.setOnClickListener {
             webView.loadUrl("https://myj.rf.gd")
+        }
+
+//        override fun onBackPressed() {
+//            if (webView.canGoBack()) {
+//                webView.goBack()
+//            }
+//            super.onBackPressed()
+//        }
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onReceivedSslError(
+                view: WebView?,
+                handler: SslErrorHandler?,
+                error: SslError?
+            ) {
+//                super.onReceivedSslError(view, handler, error)
+                handler?.proceed()
+            }
+
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                loadWebProgress.visibility = View.VISIBLE
+                webView.setOnTouchListener(OnTouchListener { v, event -> true })
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                loadWebProgress.visibility = View.INVISIBLE
+                webpageTitleTxt.setText(view?.title)
+                webView.setOnTouchListener(OnTouchListener { v, event -> false })
+            }
         }
     }
 }
