dd1019ed5a6aaad42e19cacf95235c9fbe922735
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2023-05-11 21:29:50 +0300

    Added activities

--- MYJMemoryGame2/app/src/main/java/com/yousufjamil/myjmemorygame/EasyActivity.kt
+++ MYJMemoryGame2/app/src/main/java/com/yousufjamil/myjmemorygame/EasyActivity.kt
@@ -2,6 +2,7 @@ package com.yousufjamil.myjmemorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import java.util.Random
 
 class EasyActivity : AppCompatActivity() {
@@ -11,6 +12,10 @@ class EasyActivity : AppCompatActivity() {
     var chosen1 = 0
     var chosen2 = 0
     val chosen = mutableListOf("temporary")
+    val card1easy: ImageButton = findViewById(R.id.card1easy)
+    val card2easy: ImageButton = findViewById(R.id.card2easy)
+    val card3easy: ImageButton = findViewById(R.id.card3easy)
+    val card4easy: ImageButton = findViewById(R.id.card4easy)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,5 +32,18 @@ class EasyActivity : AppCompatActivity() {
         chosen2 = random.nextInt(images.count())
         chosen.add(images[chosen2])
         println("CurrentList: $chosen")
+        assignimages(chosen)
+    }
+
+    fun assignimages(chosenlist: MutableList<String>) {
+        var countnumber1 = 0
+        var countnumber2 = 0
+        var card1asset = 0
+        var card2asset = 0
+        var card3asset = 0
+        var card4asset = 0
+        while (countnumber1+countnumber2 != 4) {
+            TODO("Will be done later")
+        }
     }
 }
