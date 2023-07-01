678952be9a5ec23312dadc6dce558f90092597aa
Muhammad Yousuf Jamil <65864658+y1234567890-eng@users.noreply.github.com>
2023-07-01 22:56:44 +0300

    Updates

---
+++ TestMail/app/src/main/java/com/yousufjamil/testmail/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.yousufjamil.testmail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//import net.axay.simplekotlinmail.delivery.send
+//import net.axay.simplekotlinmail.email.emailBuilder
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        PROJECT CANCELLED
+
+//        val email = emailBuilder {
+//            from("no-reply@example.com")
+//            to("muhammadyousufjamil@gmail.com")
+//
+//            withSubject("Test")
+//            withPlainText("Does it work?")
+//        }
+
+//        suspend fun main() = email.send()
+    }
+}
